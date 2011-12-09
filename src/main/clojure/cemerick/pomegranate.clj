de0837bdb82f55c56303cb604d06ed83d056266d
Chas Emerick <cemerick@snowtide.com>
2011-12-09 07:10:29 -0500

    attempt at limiting scope of effect of add-classpath; fix for #1?

--- src/main/clojure/cemerick/pomegranate.clj
+++ src/main/clojure/cemerick/pomegranate.clj
@@ -23,32 +23,51 @@
 
 (defprotocol AddURL
   "Ability to dynamically add urls to classloaders"
+  (can-modify? [this] "Returns true if the given classloader should be modified.")
   (add-url [this url] "add the url to the classpath"))
 
-(extend-type DynamicClassLoader AddURL
+(extend-type DynamicClassLoader
+  AddURL
+  (can-modify? [this] true)
   (add-url [this url] (.addURL this url)))
 
-(extend-type URLClassLoader AddURL
-  (add-url [this url] (call-method URLClassLoader 'addURL [URL] this url)))
+(def ^:private url-classloader-base
+  {:can-modify? (constantly true)
+   :add-url (fn [this url]
+              (call-method URLClassLoader 'addURL [URL] this url))})
 
-(defn- find-eldest-classloader
-  ([]
-    (find-eldest-classloader (clojure.lang.RT/baseLoader)))
-  ([tip]
-     (when (satisfies? AddURL tip)
-       (or (find-eldest-classloader (.getParent tip)) tip))))
+(extend URLClassLoader AddURL url-classloader-base)
+
+(defmacro when-resolves
+  [sym & body]
+  (when (resolve sym) `(do ~@body)))
+
+(when-resolves sun.misc.Launcher             
+  (extend sun.misc.Launcher$ExtClassLoader AddURL
+    (assoc url-classloader-base
+           :can-modify? (constantly false))))
+
+(defn- classloader-hierarchy
+  []
+  (->> (clojure.lang.RT/baseLoader)
+    (iterate #(.getParent %))
+    (take-while boolean)))
+
+(def ^:private suitable-classloader? #(and (satisfies? AddURL %)
+                                           (can-modify? %)))
 
 (defn add-classpath
   "A corollary to the (deprecated) `add-classpath` in clojure.core. This implementation
    requires a java.io.File or String path to a jar file or directory, and will attempt
-   to add that path to the current thread's context classloader (by default)."
+   to add that path to the right classloader (with the search rooted at the current
+   thread's context classloader)."
   ([jar-or-dir classloader]
-    (let [url (.toURL (io/file jar-or-dir))]
-      (add-url classloader url)))
+    (add-url classloader (.toURL (io/file jar-or-dir))))
   ([jar-or-dir]
-    (if-let [classloader (find-eldest-classloader)]
-      (add-classpath jar-or-dir classloader)
-      (throw (IllegalStateException. "Could not find a DynamicClassLoader or URLClassLoader to modify")))))
+    (let [classloaders (classloader-hierarchy)]
+      (if-let [cl (last (filter suitable-classloader? classloaders))]
+        (add-classpath jar-or-dir cl)
+        (throw (IllegalStateException. "Could not find a suitable classloader to modify from " classloaders))))))
 
 (defn add-dependencies
   "Resolves a set of dependencies, optionally against a set of additional Maven repositories
