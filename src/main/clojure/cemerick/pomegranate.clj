b4c5c00dcd3b4fe50c13f1054a2e134bcfda2d4a
Chas Emerick <chas@cemerick.com>
2012-02-22 05:50:10 -0500

    additional utility fns: classloader-hierarchy, modifiable-classloader?, and improvements to get-classpath

--- src/main/clojure/cemerick/pomegranate.clj
+++ src/main/clojure/cemerick/pomegranate.clj
@@ -47,14 +47,23 @@
     (assoc url-classloader-base
            :can-modify? (constantly false))))
 
-(defn- classloader-hierarchy
-  []
-  (->> (clojure.lang.RT/baseLoader)
-    (iterate #(.getParent %))
-    (take-while boolean)))
+(defn classloader-hierarchy
+  "Returns a seq of classloaders, with the tip of the hierarchy first.
+   Uses (clojure.lang.RT/baseLoader) -- which by default will be the
+   current thread context ClassLoader -- as the tip ClassLoader if one is
+   not provided."
+  ([] (classloader-hierarchy (clojure.lang.RT/baseLoader)))
+  ([tip]
+    (->> tip
+      (iterate #(.getParent %))
+      (take-while boolean))))
 
-(def ^:private suitable-classloader? #(and (satisfies? AddURL %)
-                                           (can-modify? %)))
+(defn modifiable-classloader?
+  "Returns true iff the given ClassLoader is of a type that satisfies
+   the AddURL protocol, and it can be modified."
+  [cl]
+  (and (satisfies? AddURL cl)
+       (can-modify? cl)))
 
 (defn add-classpath
   "A corollary to the (deprecated) `add-classpath` in clojure.core. This implementation
@@ -65,7 +74,7 @@
     (add-url classloader (.toURL (io/file jar-or-dir))))
   ([jar-or-dir]
     (let [classloaders (classloader-hierarchy)]
-      (if-let [cl (last (filter suitable-classloader? classloaders))]
+      (if-let [cl (last (filter modifiable-classloader? classloaders))]
         (add-classpath jar-or-dir cl)
         (throw (IllegalStateException. "Could not find a suitable classloader to modify from " classloaders))))))
 
@@ -89,10 +98,17 @@
 
 (defn get-classpath
   "Returns the effective classpath (i.e. _not_ the value of
-   (System/getProperty \"java.class.path\") as a seq of URL strings."
-  []
-  (->> (classloader-hierarchy)
-    reverse
-    (mapcat #(when (instance? URLClassLoader %) (.getURLs %)))
-    (map str)))
+   (System/getProperty \"java.class.path\") as a seq of URL strings.
+
+   Produces the classpath from all classloaders by default, or from a
+   collection of classloaders if provided.  This allows you to easily look
+   at subsets of the current classloader hierarchy, e.g.:
+
+   (get-classpath (drop 2 (classloader-hierarchy)))"
+  ([classloaders]
+    []
+    (->> (reverse classloaders)
+      (mapcat #(when (instance? URLClassLoader %) (.getURLs %)))
+      (map str)))
+  ([] (get-classpath (classloader-hierarchy))))
 
