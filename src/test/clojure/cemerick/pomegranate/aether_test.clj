e2600a8a2200144db74360bcf44f535317fe3c6d
Phil Hagelberg <phil@hagelb.org>
2017-05-18 15:22:08 -0700

    Add test for registering new wagons.
    Test based on https://github.com/xeqi/pedantic; thanks to Nelson Morris/xeqi.

--- src/test/clojure/cemerick/pomegranate/aether_test.clj
+++ src/test/clojure/cemerick/pomegranate/aether_test.clj
@@ -541,6 +541,89 @@
             ['tester "0.1.0-20120403.012847-1"] nil}
            (aether/dependency-hierarchy coords deps)))))
 
+;; taken from the test suite for the pendantic lib by Nelson Morris
+
+(defn get-versions [name repo]
+  (let [name (symbol name)]
+    (map second (filter #(= name (first %)) (keys repo)))))
+
+(defn make-pom-string [name version deps]
+  (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  <project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>" name "</groupId>
+  <artifactId>" name "</artifactId>
+  <packaging>jar</packaging>
+  <version>" version "</version>
+  <name>" name "</name>"
+  (if-not (empty? deps)
+    (apply str
+           "<dependencies>"
+           (clojure.string/join "\n"
+                                (for [[n v] deps]
+                                  (str "<dependency>
+                   <groupId>" n "</groupId>
+                   <artifactId>"n"</artifactId>
+                   <version>"v"</version>
+                   </dependency>")))
+           "</dependencies>"))
+  " </project>"))
+
+(defn make-metadata [name versions]
+  (str "<metadata>
+  <groupId>" name "</groupId>
+  <artifactId>" name "</artifactId>
+  <versioning>
+  <versions>"
+  (clojure.string/join "\n"
+                       (for [v versions]
+                         (str "<version>"v"</version>")))
+    "</versions>
+    <lastUpdated>20120810193549</lastUpdated>
+  </versioning>
+  </metadata>"))
+
+(def fake-repo
+  '{[a "1"] []
+    [a "2"] []
+    [aa "2"] [[a "2"]]})
+
+(deftest register-fake-wagon
+  (aether/register-wagon-factory!
+     "fake"
+     #(reify org.apache.maven.wagon.Wagon
+        (getRepository [_]
+          (proxy [org.apache.maven.wagon.repository.Repository] []))
+        (^void connect [_
+                        ^org.apache.maven.wagon.repository.Repository _
+                        ^org.apache.maven.wagon.authentication.AuthenticationInfo _
+                        ^org.apache.maven.wagon.proxy.ProxyInfoProvider _])
+        (disconnect [_])
+        (removeTransferListener [_ _])
+        (addTransferListener [_ _])
+        (setTimeout [_ _])
+        (setInteractive [_ _])
+        (get [_ name file]
+          (let [[n _ version] (clojure.string/split name #"/")]
+            (if (= name (str n "/" n "/maven-metadata.xml"))
+              (if-let [versions (get-versions n fake-repo)]
+                (spit file (make-metadata n versions))
+                (spit file ""))
+              (if-let [deps (fake-repo [(symbol n) version])]
+                (if (re-find #".pom$" name)
+                  (spit file (make-pom-string n version deps))
+                  (spit file ""))
+                (throw (org.apache.maven.wagon.ResourceDoesNotExistException. ""))))))))
+
+  (let [tmp-local-repo-dir (io/file tmp-dir "local-repo")]
+    (aether/resolve-dependencies :coordinates '[[a "1"]]
+                                 :repositories {"test-repo"
+                                                {:url "fake://ss"
+                                                 :checksum :ignore}}
+                                 :local-repo tmp-local-repo-dir)
+    (is (= #{"local-repo" "a" "1" "a-1.pom" "_remote.repositories" "a-1.jar"}
+           (set (map (memfn getName) (file-seq tmp-local-repo-dir)))))))
+
 (comment
   "tests needed for:
   repository authentication
