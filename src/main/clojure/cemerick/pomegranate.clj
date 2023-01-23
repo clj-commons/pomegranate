7ca3d143079f03f6f22832d9d140a000d5c97de6
lread <lee@dlread.com>
2023-01-23 11:24:40 -0500

    docs: review and freshen
    These can always be improved, but I think this is a good first pass.
    I'm also sure I made typos/mistakes, corrections are always welcome.
    
    - move from md to adoc to make the chore more pleasant for me
    - changelog now includes release dates and indications of breaking releases
    - readme is now a landing page that references change log, and user guide
      dev guide and maintainer guide
    - user guide is mostly old readme content with some elaborations
    - add `doc/cljdoc.edn` to explicitly tell cljdoc about articles
    - public API docstrings reviewed and updated
      - now markdown for better rendering on codox/cljdoc/etc
      - also helps with clarity when `args` and `types` are backticked
      - elaborated/reworded where I thought that would help
      - distinguished lower level fns from higher level ones in aether ns.
      - now more consistently referencing options defined in other fns.
      I'm sure some vars could have been internal, but I'm not about to
      change visibility at this point.
    - rename internal private `default-listener-fn` to `stdout-listener-fn`
      to better reflect its purpose
    
    Touches on #107.
    Closes #149

--- src/main/clojure/cemerick/pomegranate.clj
+++ src/main/clojure/cemerick/pomegranate.clj
@@ -1,31 +1,32 @@
 (ns cemerick.pomegranate
+  "Classpath and class loader tools."
   (:require [clojure.java.io :as io]
             [cemerick.pomegranate.aether :as aether]
             [dynapath.util :as dp])
   (:refer-clojure :exclude (add-classpath)))
 
 (defn classloader-hierarchy
-  "Returns a seq of classloaders, with the tip of the hierarchy first.
-   Uses the current thread context ClassLoader as the tip ClassLoader
-   if one is not provided."
+  "Returns a seq of class loaders, with the `tip` of the hierarchy first.
+   The `tip` defaults to the current thread context class loader."
   ([] (classloader-hierarchy (.. Thread currentThread getContextClassLoader)))
   ([tip]
     (->> tip
-      (iterate #(.getParent ^ClassLoader %))
-      (take-while boolean))))
+         (iterate #(.getParent ^ClassLoader %))
+         (take-while boolean))))
 
 (defn modifiable-classloader?
-  "Returns true iff the given ClassLoader is of a type that satisfies
-   the dynapath.dynamic-classpath/DynamicClasspath protocol, and it can
+  "Returns `true` iff the given `ClassLoader` is of a type that satisfies
+   the `dynapath.dynamic-classpath/DynamicClasspath` protocol, and can
    be modified."
   [cl]
   (dp/addable-classpath? cl))
 
 (defn add-classpath
-  "A corollary to the (deprecated) `add-classpath` in clojure.core. This implementation
-   requires a java.io.File or String path to a jar file or directory, and will attempt
-   to add that path to the right classloader (with the search rooted at the current
-   thread's context classloader)."
+  "A corollary to the deprecated `add-classpath` in `clojure.core`.
+
+   Attempt to add `jar-or-dir`, a string or `java.io.File`, to `classloader`.
+
+   When `classloader` is not provided, searches for a modifiable classloader rooted at the current thread's context classloader."
   ([jar-or-dir classloader]
      (when-not (dp/add-classpath-url classloader (.toURL (.toURI (io/file jar-or-dir))))
        (throw (IllegalStateException. (str classloader " is not a modifiable classloader")))))
@@ -37,58 +38,56 @@
                                             classloaders)))))))
 
 (defn add-dependencies
-  "Resolves a set of dependencies, optionally against a set of additional Maven
-   repositories, and adds all of the resulting artifacts (jar files) to the
-   current runtime via `add-classpath`:
+  "Resolves dependency `:coordinates` against Maven `:repositories`, then adds all
+   the resulting artifacts (jar files) to the current runtime via [[add-classpath]]:
 
+   ```Clojure
    (add-dependencies :classloader your-classloader
-                     :coordinates '[[incanter \"1.2.3\"]]
+                     :coordinates '[[incanter \"1.9.2\"]]
                      :repositories (merge cemerick.pomegranate.aether/maven-central
-                                     {\"clojars\" \"https://clojars.org/repo\"}))
+                                          {\"clojars\" \"https://clojars.org/repo\"}))
+   ```
+ 
+   kwarg options:
+   - `:classloader` - (optional, defaults to closest modifiable classloader in current thread's
+   hierarchy as per [[add-classpath]])
+   - Otherwise, kwargs are the same as [[cemerick.pomegranate.aether/resolve-dependencies]]
 
-   Note that the `:classloader` kwarg is optional; if not provided then resolved
-   dependencies will be added to the closest modifiable classloader in the
-   current thread's hierarchy, as per `add-classpath`.
-
-   Otherwise, acceptable arguments are the same as those for
-   `cemerick.pomegranate.aether/resolve-dependencies`; returns the dependency graph
-   returned from that function.
-
-   Note that Maven central is used as the sole repository if none are specified.
-   If :repositories are provided, then you must merge in the `maven-central` map from
-   the cemerick.pomegranate.aether namespace yourself."
-  [& args]
-  (let [classloader (-> (apply hash-map args)
+   Returns the dependency graph from [[cemerick.pomegranate.aether/resolve-dependencies]]."
+  [& kwargs]
+  (let [classloader (-> (apply hash-map kwargs)
                         :classloader
                         ; replace with some-> when we bump the clojure dep
                         (#(when % [%])))
-        deps (apply aether/resolve-dependencies args)]
+        deps (apply aether/resolve-dependencies kwargs)]
     (doseq [artifact-file (aether/dependency-files deps)]
       (apply add-classpath artifact-file classloader))
     deps))
 
 (defn get-classpath
   "Returns the effective classpath (i.e. _not_ the value of
-   (System/getProperty \"java.class.path\") as a seq of URL strings.
+   `(System/getProperty \"java.class.path\")` as a seq of URL strings.
 
    Produces the classpath from all classloaders by default, or from a
-   collection of classloaders if provided.  This allows you to easily look
+   collection of `classloaders` if provided.  This allows you to easily look
    at subsets of the current classloader hierarchy, e.g.:
 
-   (get-classpath (drop 2 (classloader-hierarchy)))"
+   ```Clojure
+   (get-classpath (drop 2 (classloader-hierarchy)))
+   ```"
   ([classloaders]
     (->> (reverse classloaders)
-      (mapcat #(dp/classpath-urls %))
-      (map str)))
+         (mapcat #(dp/classpath-urls %))
+         (map str)))
   ([] (get-classpath (classloader-hierarchy))))
 
 (defn classloader-resources
-  "Returns a sequence of [classloader url-seq] pairs representing all
-   of the resources of the specified name on the classpath of each
-   classloader. If no classloaders are given, uses the
-   classloader-heirarchy, in which case the order of pairs will be
-   such that the first url mentioned will in most circumstances match
-   what clojure.java.io/resource returns."
+  "Returns a sequence of `[java.lang.ClassLoader [java.net.URL ...]]` pairs
+   for all found `resource-name`s on the classpath of each classloader.
+
+   If no `classloaders` are given, uses the [[classloader-hierarchy]].
+   In this case, the first URL will in most circumstances match what
+   what `clojure.java.io/resource` returns."
   ([classloaders resource-name]
      (for [classloader (reverse classloaders)]
        [classloader (enumeration-seq
@@ -96,11 +95,10 @@
   ([resource-name] (classloader-resources (classloader-hierarchy) resource-name)))
 
 (defn resources
-  "Returns a sequence of URLs representing all of the resources of the
-   specified name on the effective classpath. This can be useful for
-   finding name collisions among items on the classpath. In most
+  "Returns a sequence of `java.net.URL`s on the effective classpath for specified specified `resource-name`.
+   This can be useful for finding name collisions among items on the classpath. In most
    circumstances, the first of the returned sequence will be the same
-   as what clojure.java.io/resource returns."
+   as what `clojure.java.io/resource` returns."
   ([classloaders resource-name]
      (distinct (mapcat second (classloader-resources classloaders resource-name))))
   ([resource-name] (resources (classloader-hierarchy) resource-name)))
