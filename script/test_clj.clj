6ae51ca14763393cafaf0cbb33e6271ab96b0f31
lread <lee@dlread.com>
2023-01-29 17:41:56 -0500

    tests: restore testing with old Clojure versions
    Closes #156

--- script/test_clj.clj
+++ script/test_clj.clj
@@ -4,13 +4,15 @@
             [lread.status-line :as status]))
 
 (defn -main [& args]
-  (let [all-clojure-versions ["1.8" "1.9" "1.10" "1.11"]
+  (let [old-clojure-versions ["1.4" "1.5" "1.6" "1.7"]
+        all-clojure-versions (concat old-clojure-versions ["1.8" "1.9" "1.10" "1.11"])
+        default-version (first all-clojure-versions)
         valid-clj-version-opt-values (conj all-clojure-versions ":all")
         spec {:clj-version
               {:ref "<version>"
                :desc "The Clojure version to test against."
                :coerce :string
-               :default-desc "1.8"
+               :default-desc default-version
                ;; don't specify :default, we want to know if the user passed this option in
                :validate
                {:pred (set valid-clj-version-opt-values)
@@ -27,7 +29,7 @@
                             (recur (drop 2 args) out-args)
                             (recur (rest args) (conj out-args a)))
                           out-args)))
-        clj-version (or clj-version "1.8")]
+        clj-version (or clj-version default-version)]
 
     (if (:help opts)
       (do
@@ -38,9 +40,12 @@
       (let [clj-versions (if (= ":all" clj-version)
                            all-clojure-versions
                            [clj-version])]
-        (doseq [v clj-versions]
+        (doseq [v clj-versions
+                :let [test-alias (if (some #{v} old-clojure-versions)
+                                   "test:old-runner"
+                                   "test")]]
           (status/line :head "Testing against Clojure version %s" v)
-          (apply t/clojure (format "-M:%s:test" v) runner-args))))))
+          (apply t/clojure (format "-M:%s:%s" v test-alias) runner-args))))))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
