b5e28716bf8e2cd85acbc070fe7564cb05525a59
Lee Read <lee@dlread.com>
2023-02-05 16:05:25 -0500

    dev: automate publishing of a release (#160)
    Invoked via `bb publish`.
    Read new doc/03-maintainer-guide.adoc for details.
    
    Based on publish workflow from clj-commons/clj-yaml, then tweaked to:
    - add more checks before publishing (must not be on fork, head shas must match)
    - generalized a bit so I can more easily steal this work for the next project
    
    Changes of note:
    - now using tools build instead of depstar to create the jar
    - moved to anemic pom.xml, our deps are now driven by deps.edn, full pom
    for deploy is generated during publish workflow
    - using babashka/neil convention of storing version in deps.edn
    - git tagging release with v1.2.3 instead of Release-1.2.3
    - version patch is now release count over the life of Pomegranate.
      Maven central had 19, and clojars had 3, so the next release should be 1.2.23.
    - pom now specifies the minimum supported version of Clojure (currently 1.4.0)
    
    Could be further tweaks required after the first attempt of a publish, but looks pretty good.
    
    Closes #160, closes #136.

---
+++ build.clj
@@ -0,0 +1,43 @@
+(ns build
+  (:require [build-shared]
+            [clojure.tools.build.api :as b]))
+
+(def version (build-shared/lib-version))
+(def lib (build-shared/lib-artifact-name))
+
+;; build constants
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn jar [_]
+  (println "jarring version" version)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :scm {:tag (build-shared/version->tag version)}
+                :basis basis
+                :src-dirs ["src/main/clojure"]})
+  (b/copy-dir {:src-dirs ["src/main/clojure"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn install [_]
+  (jar {})
+  (println "installing version" version)
+  (b/install {:basis basis
+              :lib lib
+              :version version
+              :jar-file jar-file
+              :class-dir class-dir}))
+
+(defn deploy [opts]
+  (jar opts)
+  (println "deploy")
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   (merge {:installer :remote
+           :artifact jar-file
+           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
+  opts)
